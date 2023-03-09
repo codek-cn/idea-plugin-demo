22ba4c2e06c0449a7bb7c12561cf0abca8eeb409
GitHub Action <action@github.com>
2023-03-09 08:45:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/codekcn/ideaplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/codekcn/ideaplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.codekcn.ideaplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.codekcn.ideaplugindemo.MyBundle
 
 class MyApplicationService {
 
